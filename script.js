bfc77c7acd1aad2b9f5434c1d646e36c40bed0fb
JoshFoRs <116329299+JoshFoRs@users.noreply.github.com>
2022-10-21 14:22:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -100,8 +100,7 @@ window.addEventListener("message", function (event) {
   for (var z = 0; z < event.data.traits.length; z++) {
     var textArea = checkContent("textarea", "name", "features", false)[0];
     textArea.innerHTML += event.data.traits[z].trait + "\r\n";
-    textArea.innerHTML +=
-      "Class: " + event.data.traits[z].classType + "\r\n\r\n";
+    textArea.innerHTML += event.data.traits[z].classType + "\r\n\r\n";
   }
 
   if (
